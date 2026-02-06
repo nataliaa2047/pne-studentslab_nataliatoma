743f4f3e6e80cb3dcc99aedad9c0d93a545e04f9
nataliaa2047 <n.toma.2024@alumnos.urjc.es>
2026-02-06 19:08:33 +0100

    I've finished doing exercise 1 of the session 2.

--- S02/fibonacci.py
+++ S02/fibonacci.py
@@ -1,7 +1,7 @@
-sequence = [0,1]
-for i in range(2,11):
-    fic = sequence[-1] + sequence[-2]
-    sequence.append(fic)
-for item in sequence:
-    print(item, end=" ")
+sequence = [0,1]        #lista que representa los dos primeros números de Fibonacci
+for i in range(2,11):     #se repite sólo 9 veces porque el 0 y el 1 y los tenemos incluidos de antes, así que ya tenemos 11 números
+    fic = sequence[-1] + sequence[-2]  #suma del último más penúltimo número de la lista
+    sequence.append(fic)    #se añade lo nuevo calculado al final de la lista
+for item in sequence:    #recorre la lista uno por uno
+    print(item, end=" ")   #imprime cada número en la misma línea
 
