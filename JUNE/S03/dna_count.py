99d92a402c779f30069186271711d0aebe1d7dc7
nataliaa2047 <n.toma.2024@alumnos.urjc.es>
2026-06-20 19:38:36 +0200

    SESSION 3

---
+++ JUNE/S03/dna_count.py
@@ -0,0 +1,23 @@
+def bases_counter(seq):
+    count_a = 0
+    count_c = 0
+    count_g = 0
+    count_t = 0
+    for i in seq:
+        if i == "A":
+            count_a += 1
+        if i == "C":
+            count_c += 1
+        if i == "G":
+            count_g += 1
+        if i == "T":
+            count_t += 1
+    return count_a, count_c, count_g, count_t
+
+sequence = input("Please, enter a sequence: ")
+print(f"Introduce the sequence: {sequence}")
+print(f"Total length: {len(sequence)}")
+print(f"A: {bases_counter(sequence)[0]}")
+print(f"C: {bases_counter(sequence)[1]}")
+print(f"G: {bases_counter(sequence)[2]}")
+print(f"T: {bases_counter(sequence)[3]}")
